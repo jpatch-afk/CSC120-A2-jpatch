5bff016893617df178c699d828a6c73e52c4a624
jpatch-afk <jpaczkowski@smith.edu>
2024-10-10 10:24:39 -0400

    Update computer.py to current version

--- computer.py
+++ computer.py
@@ -1,45 +1,39 @@
 class Computer:
-
-    description: str
-    processor_type: str
-    hard_drive_capacity: int
-    memory: int
-    operating_system: str
-    year_made: int
-    price: int
-    new_price: int
+    computer_des:str
+    processor_type:str
+    hard_drive_capacity:int
+    computer_memory:int
+    operating_system:str
+    year_made:int
+    price:int
     item_id:int 
 
-    def __init__(self, description: str, processor_type: str,hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int, new_price:int, item_id: int):
-        self.description = description
+    #Constructor
+    def __init__(self, computer_des:str, processor_type:str, hard_drive_capacity:int, computer_memory:int, operating_system:str, year_made:int, price:int, item_id:int):
+        self.computer_des = computer_des
         self.processor_type = processor_type
         self.hard_drive_capacity = hard_drive_capacity
-        self.memory = memory
+        self.computer_memory = computer_memory
         self.operating_system = operating_system
         self.year_made = year_made
         self.price = price
-        self.new_price = new_price
         self.item_id = item_id
-    
-    #def update price
 
+  #Updates the price of the computer instance in the array 
     def update_price(self, new_price:int, item_id:int):
-        pass 
+        print("Changing price...")
+        self.price = new_price 
+        print("Price changed successfully!")
 
+    #Updates the operating system of the computer instance in the area 
+    def update_OS(self, operating_system:str, new_OS:int, item_id:int):
+        print("Changing OS...")
+        self.operating_system = new_OS
+        print("OS changed successfully!")
     
-
-
-    #def update OS
-
-
-
-
+#Creates an instance of a computer in the main 
 def main():
-    computer = Computer(
-    "Mac Pro (Late 2013)",
-    "3.5 GHc 6-Core Intel Xeon E5",
-    1024, 64,
-    "macOS Big Sur", 2013, 1500)
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500, 1)
 
-if __name__ == "__main__":   #only call main if I am running this program directly
+if __name__ == "__main__": 
     main()
