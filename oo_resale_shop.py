8d33cb5350011f1c778814f8beb3a807832a7762
jpatch-afk <jpaczkowski@smith.edu>
2024-10-10 10:26:20 -0400

    Update oo_resale_shop.py to current version

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,39 +1,44 @@
 class ResaleShop:
+ 
+    inventory:list
 
-    item_ID: int 
-    inventory: list
-    new_os: str 
+    def __init__(self, inventory:list):
+        self.inventory = inventory
+        inventory = [""] 
 
+    #Sells a computer, or removes it from the store inventory 
+    def sell (self, c, inventory:list):
+        if (c in inventory): 
+            print("Trying to remove", c, "from inventory...")
+            inventory.remove(c)
+            print("Success!")
+        else: 
+            print(f"{c} is not in the inventory!")
 
-    def __init__(self, item_ID: int, inventory:list, new_os:str):
-        self.item_ID = item_ID 
-        self.inventory = [] 
-        self.new_os = new_os
+   #Refurbishes the computer based on the year it's made 
+    def refurbish (self, c, new_os: str, inventory):
+        if (c in inventory):
+            print("Trying to refurbish item...") 
+        else:
+            print(f"{c} is not in the inventory!")
 
-    def sell (self, c):
-        print("Trying to remove", c, "from inventory...")
-        self.inventory.remove(c)
-        print("Success!")
-
-
-    def refurbish (self, c, new_os: str):
-        print("Trying to refurbish item",c)
-        
-        
-
-    #GOAL: add c to self.inventory
+    #Buys a computer, or adds a computer to the inventory 
     def buy(self, c):
         print("Trying to add", c, "to inventory...")
         self.inventory.append(c)
         print("Success!")
 
+#Prints the inventory 
+    def printinventory(inventory:list):
+        print(inventory)
 
+#Defines a shop that using the ResaleShop class and simulates transactions 
 def main():
     myShop: ResaleShop = ResaleShop()
-    print("There are", len(myShop.inventory), "items in stock.")
+    print("There are ", len(myShop.inventory), "item(s) in stock.")
     c = "MY COMPUTER"
     myShop.buy(c)
-    print("There are now", len(myShop.inventory), "items in stock.")
+    print("There are ", len(myShop.inventory), "item(s) in stock.")
     
 
 if __name__ == "__main__":
